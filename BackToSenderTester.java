5ab684911a4c10733ca6060bb8827cdc37684c91
FUEGO62 <jesseolaisrael@gmail.com>
2024-09-04 09:06:08 +0100

    add BackToSenderTester.java file

--- BackToSenderTester.java
+++ BackToSenderTester.java
@@ -6,13 +6,25 @@ public static void main(String[]args){
 
 Scanner input = new Scanner(System.in);
 
-System.out.print("how many parcels did you deliver");
+try{
+
+System.out.print("how many parcels did you deliver: ");
 
 int number = input.nextInt();
+
+
 
 int wage = BackToSender.WageCalculator(number);
 
 System.out.print(wage);
+
+}
+
+catch(Exception e){
+
+System.out.print("Enter a positive whole number !!!");
+
+}
 
 }
 
